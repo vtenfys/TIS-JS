b26afe6bf29736de28ec67bff43c3b5d398ea9e5
David Bailey <david.bailey@archlinux.net>
2016-10-02 17:57:25 +0100

    Limit chars per line, add status divs to nodes and update sprites

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ for (let i = 0; i < 12; i += 1) {
 
   node[i].classList.add("node");
   node[i].code = document.createElement("textarea");
+
   node[i].code.spellcheck = false;
+  node[i].code.addEventListener("input", function () {
+    var text = this.value.split("\n");
+    for (let i = 0; i < text.length; i += 1) {
+      if (text[i].length > 18) text[i] = text[i].substring(0, 18);
+    }
+    this.value = text.join('\n');
+  });
+
   node[i].appendChild(node[i].code);
+
+  for (let thing of ["acc", "bak", "last", "mode", "idle"]) {
+    node[i][thing] = document.createElement("div");
+    node[i][thing].classList.add("data", thing);
+    node[i].appendChild(node[i][thing]);
+  }
 
   if (i > 3) {
     node[i].classList.add("margin-top");
