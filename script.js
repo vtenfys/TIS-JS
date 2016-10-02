1bb791c2963be1e9131dd727fb0c99e9fbb5cdf9
David Bailey <david.bailey@archlinux.net>
2016-10-02 11:35:01 +0100

    Add arrows and CSS bugfixes

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ for (let i = 0; i < 12; i += 1) {
   node[i].code.spellcheck = false;
   node[i].appendChild(node[i].code);
 
-  if (i > 3) node[i].classList.add("margin-top");
-  if (i % 4 !== 0) node[i].classList.add("margin-left");
+  if (i > 3) {
+    node[i].classList.add("margin-top");
+    node[i].arrow = {};
+
+    for (let dir of ["up", "down"]) {
+      node[i].arrow[dir] = document.createElement("div");
+      node[i].arrow[dir].classList.add("arrow", dir);
+      node[i].appendChild(node[i].arrow[dir]);
+    }
+  }
+  if (i % 4 !== 0) {
+    node[i].classList.add("margin-left");
+    if (!node[i].arrow) node[i].arrow = {};
+
+    for (let dir of ["left", "right"]) {
+      node[i].arrow[dir] = document.createElement("div");
+      node[i].arrow[dir].classList.add("arrow", dir);
+      node[i].appendChild(node[i].arrow[dir]);
+    }
+  }
 }
+
+var TIS = {};
