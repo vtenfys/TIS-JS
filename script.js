c6cb11bc5b0004b7541da6d02026dfe4fc08a94f
David Bailey <david.bailey@archlinux.net>
2016-10-03 06:35:22 +0100

    Add MOV support to API

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ var save = [];
 for (let i = 0; i < 12; i += 1) {
   node[i] = document.createElement("div");
   document.querySelector("#grid").appendChild(node[i]);
-
   node[i].classList.add("node");
-  node[i].code = document.createElement("textarea");
 
+  node[i].outgate = {};
+  node[i].ready = true;
+
+  node[i].code = document.createElement("textarea");
   node[i].code.spellcheck = false;
+
   node[i].code.addEventListener("input", function () {
     var text = this.value.split("\n");
     for (let i = 0; i < text.length; i += 1) {
@@ -68,7 +71,7 @@ if (window.name !== "") {
 }
 
 var TIS = {
-  runCmd: function (cmd, node) {
+  eval: function (nodei, cmd) {
     cmd = cmd.replace(/((\s)\s+)|,/g, "$2").split(" ");
     if ((!cmd[0][0]) || cmd[0][0] === "#") return;
 
@@ -87,6 +90,59 @@ var TIS = {
     if (cmd[0] === "mov") {
       if (cmd.length < 3) throw new SyntaxError("missing operand");
       if (cmd.length > 3) throw new SyntaxError("too many operands");
+      TIS.mov(nodei, cmd[1], cmd[2]);
+    }
+  },
+  mov: function (nodei, src, dst) {
+    if (src.match(/^[0-9]+$/)) {
+      if (["up", "right", "down", "left"].indexOf(dst) !== -1) {
+        if (node[nodei].outgate[dst] && node[nodei].outgate[dst].match(/a-z/)) {
+          if (dst === "up")         var dstnode = nodei - 4;
+          else if (dst === "right") var dstnode = nodei + 1;
+          else if (dst === "down")  var dstnode = nodei + 4;
+          else                      var dstnode = nodei - 1;
+          TIS.mov(dstnode, src, node[nodei].outgate[dst]);
+          node[dstnode].ready = true;
+        } else {
+          node[nodei].outgate[dst] = src;
+          node[nodei].ready = false;
+        }
+      } else if (dst === "acc") {
+        node[nodei].acc.setAttribute("data-value", src);
+      } else {
+        throw new ReferenceError('invalid register "' + dst + '"');
+      }
+    } else if (["up", "right", "down", "left"].indexOf(src) !== -1) {
+      if (src === "up") {
+        var rule = nodei > 3;
+        var dstnode = nodei - 4;
+        var dstdir = "down";
+      } else if (src === "right") {
+        var rule = nodei < 11;
+        var dstnode = nodei + 1;
+        var dstdir = "left";
+      } else if (src === "down") {
+        var rule = nodei < 8;
+        var dstnode = nodei + 4;
+        var dstdir = "up";
+      } else {
+        var rule = nodei > 0;
+        var dstnode = nodei - 1;
+        var dstdir = "right";
+      }
+      if (rule) {
+        if (node[dstnode].outgate[dstdir]) {
+          TIS.mov(nodei, node[dstnode].outgate[dstdir], dst);
+          node[dstnode].ready = true;
+        } else {
+          node[dstnode].outgate[dstdir] = dst;
+          node[nodei].ready = false;
+        }
+      }
+    } else if (src === "acc") {
+      TIS.mov(nodei, node[nodei].acc.getAttribute("data-value"), dst);
+    } else {
+      throw new SyntaxError('invalid expression "' + src + '"');
     }
   }
 };
